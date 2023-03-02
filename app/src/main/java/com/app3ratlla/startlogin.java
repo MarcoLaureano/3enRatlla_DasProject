bd5b7e98936add1a90be5b0384b89b96d1f996ed
MarcoLaureano <mlaurean19@jaumebalmes.net>
2023-03-02 08:37:35 +0100

    first commit

---
+++ app/src/main/java/com/app3ratlla/startlogin.java
@@ -0,0 +1,14 @@
+package com.app3ratlla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginstart);
+    }
+}
